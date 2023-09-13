b0dda58a8f673f5dd8c99dc161551f344a4c6db7
Vildan Softic <zewa666@gmail.com>
2023-09-13 22:58:21 +0200

    example of an attribute selector for state slices

---
+++ NLend/InjectState.cs
@@ -0,0 +1,14 @@
+﻿namespace NLendApp
+{
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = false)]
+	public class InjectState<T> : Attribute
+	{
+        public string Selector { get; set; }
+        public Type Type { get; set; }
+        public InjectState(string selector)
+        {
+            Selector = selector;
+            Type = typeof(T);
+        }
+    }
+}
